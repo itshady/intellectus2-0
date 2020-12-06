92e4634be02701b9d8b64fe1cc2da5242135b7a7
itshady <hadyib007@gmail.com>
2020-12-06 17:56:22 -0500

    add video support

--- js/modal-image.js
+++ js/modal-image.js
@@ -8,43 +8,54 @@ $(document).ready(function () {
         var locationjpg = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpg';
         var locationjpeg = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpeg';
         var locationpng = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.png';
+        var locationmp4 = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.mp4';
         //if (btnname+'jpeg') {
             //$('.modal-body').append('<img id="ExImg" alt="Content to come soon..." src="./images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpg" />')
         //}
         //if ('./images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpg' == true){
-            document.getElementById("ExImg").src = locationjpg;
+            //document.getElementById("ExImg").src = locationjpg;
+            //document.getElementById("submodal-video").src = locationmp4;
+            
+            
+
         //}
             //console.log(location.exists());
         //console.log(foldername);
         //console.log(subfoldername);
+
+        document.getElementById("submodal-video").remove();
+        $("#ExImg").css("display", "block");
 
         document.getElementById("title-up").innerHTML = btnname;
         
         $.get(locationjpg)
             .done(function() { 
                 // exists code 
-                console.log("exists");
                 document.getElementById("ExImg").src = locationjpg;
             }).fail(function() { 
                 // not exists code
-                console.log("doesnt exist");
                 $.get(locationjpeg)
                     .done(function() { 
                         // exists code 
-                        console.log("exists");
                         document.getElementById("ExImg").src = locationjpeg;
                     }).fail(function() { 
                         // not exists code
-                        console.log("doesnt exist");
                         $.get(locationpng)
                             .done(function() { 
                                 // exists code 
-                                console.log("exists");
                                 document.getElementById("ExImg").src = locationpng;
                             }).fail(function() { 
                                 // not exists code
-                                console.log("doesnt exist");
+                                $.get(locationmp4)
+                                    .done(function() { 
+                                        // exists code 
+                                        $("#modal-video").css("display", "none");
+                                        $("#modal-video").css("display", "");
+                                        $('#modal-video').append('<source id="submodal-video"' + locationmp4 + 'type="video/mp4">')
+                                    }).fail(function() { 
+                                        // not exists code
 
+                                    })
                             })
                     })
             })
