575a602116e9db1b7b8350bb4c27ff555029bb0f
itshady <hadyib007@gmail.com>
2020-12-08 13:37:58 -0500

    good copy of whole website, including images

--- js/modal-image.js
+++ js/modal-image.js
@@ -1,14 +1,19 @@
 // JavaScript source code
     
 $(document).ready(function () { 
+
+    var grade = $('.col-md-12 > h3').text();
+    var subject = $('.col-md-12 > h1').text();
+    $('.breadcrumbs').append('<span>' + grade + ' ' + subject + '</span>');
+
     $('.unit-button').click(function() {
         var btnname = $(this).text();
         var foldername = $('.col-md-12 > h3').text();
         var subfoldername = $('.col-md-12 > h1').text();
-        var locationjpg = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpg';
-        var locationjpeg = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpeg';
-        var locationpng = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.png';
-        var locationmp4 = './images/' + foldername + '/' + subfoldername + '/' + btnname + '.mp4';
+        var locationjpg = '../images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpg';
+        var locationjpeg = '../images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpeg';
+        var locationpng = '../images/' + foldername + '/' + subfoldername + '/' + btnname + '.png';
+        var locationmp4 = '../images/' + foldername + '/' + subfoldername + '/' + btnname + '.mp4';
         //if (btnname+'jpeg') {
             //$('.modal-body').append('<img id="ExImg" alt="Content to come soon..." src="./images/' + foldername + '/' + subfoldername + '/' + btnname + '.jpg" />')
         //}
@@ -16,15 +21,7 @@ $(document).ready(function () {
             //document.getElementById("ExImg").src = locationjpg;
             //document.getElementById("submodal-video").src = locationmp4;
 
-
-        //document.getElementById("#ExVideo").src = "https://www.youtube.com/embed/tgbNymZ7vqY";
-
-        //}
-            //console.log(location.exists());
-        //console.log(foldername);
-        //console.log(subfoldername);
-        $("#ExImg").css("display", "block");
-        $("#ExVideo").css("display", "none");
+            document.getElementById("ExImg").src = locationjpg;
 
         document.getElementById("title-up").innerHTML = btnname;
         
@@ -45,15 +42,12 @@ $(document).ready(function () {
                                 // exists code 
                                 document.getElementById("ExImg").src = locationpng;
                             }).fail(function() { 
-                                // not exists code
-                                $("#ExImg").css("display", "none");
-                                $("#ExVideo").css("display", "");
-                                document.getElementById("#ExVideo").src = "https://www.youtube.com/embed/tgbNymZ7vqY";
+
                             })
                     })
             })
 
 
     });
-
 });
+
